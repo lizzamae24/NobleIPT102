e2c5166e24cd5af0ac735aab3ae26317c2d5e480
lizzamae24 <143572783+lizzamae24@users.noreply.github.com>
2026-02-28 20:03:14 +0800

    Add files via upload

---
+++ NobleIPT102WPF/NobleFinalDomain/Commands/AddSensorCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NobleFinalDomain.Commands
+{
+    public class AddSensorCommand
+    {
+        public string Name { get; set; }
+        public string SensorType { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ NobleIPT102WPF/NobleFinalDomain/Queries/GetSensorByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NobleFinalDomain.Queries
+{
+    public class GetSensorByIdQuery
+    {
+        public int Id { get; set; }
+    }
+}
